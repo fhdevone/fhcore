c3876dc7d95c2dd92a74a413742e8a551a8cf485
Hammer Boy <hammerboy@tingis.local>
2013-03-06 10:25:47 -0500

    Still getting basic build framework in place.

---
+++ fhcore.cpp
@@ -0,0 +1,40 @@
+#define LED 13
+
+int step;
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+  step = 0;
+}
+
+void loop()
+{
+  step++;
+  Serial.print("Begining step ");
+  Serial.println(step);
+  digitalWrite(LED, HIGH);
+  delay(300);
+  digitalWrite(LED, LOW);
+  delay(200);
+  digitalWrite(LED, HIGH);
+  delay(300);
+  digitalWrite(LED, LOW);
+  delay(200);
+  digitalWrite(LED, HIGH);
+  delay(300);
+  digitalWrite(LED, LOW);
+  delay(200);
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(200);
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(200);
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(200);
+}
